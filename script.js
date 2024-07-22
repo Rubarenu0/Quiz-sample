85dc31144918532ca95a8fb2107f6c5d3cca48dc
Ruba Renugavathi A <142423853+Rubarenu0@users.noreply.github.com>
2024-07-22 18:33:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var div=document.getElementsByClassName("optionlist")
+var correctAnswer=question.answer;
+function userAns1(){
+    document.getElementsByClassName("demo1")[0].style.backgroundColor="red";
+}
+function userAns2(){
+    document.getElementsByClassName("demo2")[0].style.backgroundColor="red";
+}
+function userAns3(){
+    document.getElementsByClassName("demo3")[0].style.backgroundColor="red";
+}
+
+function correctAns(){
+    document.getElementsByClassName("demo4")[0].style.backgroundColor="green";
+}
